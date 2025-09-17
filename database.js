ef7452906a394127f4b47508742c8874c6774b13
Choguileague2025 <increativear@gmail.com>
2025-09-17 07:38:58 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg');
 
+console.log('=== DATABASE CONFIG DEBUG ===');
+console.log('DATABASE_URL exists:', !!process.env.DATABASE_URL);
+console.log('DATABASE_URL starts with:', process.env.DATABASE_URL ? process.env.DATABASE_URL.substring(0, 20) + '...' : 'UNDEFINED');
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
