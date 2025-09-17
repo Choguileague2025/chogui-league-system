977375f4cac23c88b915267f8d402f99f1ebbca2
Choguileague2025 <increativear@gmail.com>
2025-09-16 21:48:39 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,21 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Usar solo la DATABASE_URL completa
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    }
-});
-
-// Probar la conexión
-pool.connect((err, client, release) => {
-    if (err) {
-        console.error('❌ Error conectando a PostgreSQL:', err.stack);
-    } else {
-        console.log('✅ Conectado a PostgreSQL correctamente');
-        release();
     }
 });
 
